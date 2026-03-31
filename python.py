6951d2a7a25888043a96fe5d5bd4bf06bad42b7a
Gautam989coder <akgautamgk@gmail.com>
2026-04-01 01:03:14 +0530

    Add Even Leap Year program

--- python.py
+++ python.py
@@ -4,3 +4,18 @@ if num % 2 == 0:
     print("The number is even.")
 else:
     print("The number is odd.") 
+
+
+
+#--------------------------------------------------------------
+#Write a program to find Leap Year or not.
+year = int(input("Enter a year: "))     
+if year % 4 == 0 :
+    print(year, "is a leap year.")
+elif year % 100 == 0:
+    print(year, "is not a leap year.")      
+elif year % 400 == 0:
+    print(year, "is a leap year.")  
+else:    
+    print(year, "is not a leap year.")
+    
