23d9b567c6b6b9a2f0a877be4fe0446d94ac13d8
Gautam989coder <akgautamgk@gmail.com>
2026-04-01 00:41:40 +0530

    Add Even odd  program

---
+++ python.py
@@ -0,0 +1,6 @@
+#Write a program to find the number is even or odd.
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.") 
