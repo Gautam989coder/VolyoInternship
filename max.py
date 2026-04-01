115865acca7f30fa9d09c7759a058c7d49bdc290
Gautam989coder <akgautamgk@gmail.com>
2026-04-01 10:22:16 +0530

    Add program switch days

--- max.py
+++ max.py
@@ -1,12 +1,37 @@
 #write a program to find the maximum of three numbers
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))    
-num3 = float(input("Enter third number: "))
-if (num1 >= num2) and (num1 >= num3):
-   print("The largest number is", num1)
-elif (num2 >= num1) and (num2 >= num3):
-   print("The largest number is", num2)
-else:
-    print("The largest number is", num3)
+from unittest import case
 
+
+a = float(input("Enter first number: "))
+b= float(input("Enter second number: "))    
+c = float(input("Enter third number: "))
+if (a >= b) and (a >= c):
+   print("The largest number is ", a)
+elif (b >=a) and (b >= c):
+   print("The largest number is", b)
+else:
+    print("The largest number is", c)
+
+
+
+#--------------------------------------------------------
+#Write a program to switch case of number
+day = int(input("Enter a number (1-7): "))
+match day:  
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:     
+        print("Invalid input! Please enter a number between 1 and 7.")
